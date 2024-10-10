d4571ba3bf4d30f5cd9b31d02073f19af8636bc7
brinq <andrewrdavis18@gmail.com>
2024-10-10 08:24:59 -0700

    added a basic logging system

---
+++ include/log.h
@@ -0,0 +1,61 @@
+#pragma once
+#include "global.h"
+
+#define INFO 689
+#define WARN 690
+#define ERROR 691
+#define CRITICAL 692
+
+#define CRITICAL_OUTPUT_FORMAT "\033[1m\033[31m" << "CRITICAL" << ": " << "\033[0m"
+#define ERROR_OUTPUT_FORMAT "\033[1m\033[33m" << "ERROR" << ": " << "\033[0m"
+#define WARN_OUTPUT_FORMAT "\033[1m\033[34m" << "WARN" << ": " << "\033[0m"
+#define INFO_OUTPUT_FORMAT "\033[1m\033[36m" << "INFO" << ": " << "\033[0m"
+#define UNKNOWN_OUTPUT_FORMAT "\033[1m\033[35m" << "UNKOWN LOG TYPE" << ": " << "\033[0m"
+
+#define LOG(severity, msg)  LOGGING::addLog(severity, msg);
+
+
+namespace LOGGING{
+
+struct LogMessage{
+  const char* pMsg;
+  int severity;
+};
+
+static std::vector<LogMessage> logArray;
+
+inline void init(){
+  logArray.reserve(40);
+}
+
+inline void addLog(int severity, const char* msg){
+  logArray.emplace_back(LogMessage{msg, severity});
+}
+
+inline void logToOutputStdout(){
+  for(const LogMessage& log: logArray ){
+    switch(log.severity){
+    case INFO:
+      std::cout<< INFO_OUTPUT_FORMAT << log.pMsg << std::endl;
+      break;
+    case WARN:
+      std::cout<< WARN_OUTPUT_FORMAT << log.pMsg << std::endl;
+      break;
+    case ERROR:
+      std::cout<< ERROR_OUTPUT_FORMAT << log.pMsg << std::endl;
+      break;
+    case CRITICAL:
+      std::cout<< CRITICAL_OUTPUT_FORMAT << log.pMsg << std::endl;
+      break;
+    default:
+        std::cout<< ERROR_OUTPUT_FORMAT << log.pMsg << std::endl;
+        break;
+    }
+  }
+}
+
+inline void flushLogs(){
+  logArray.clear();
+}
+
+}
--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,10 +1,16 @@
 #include "engine.h"
 #include "window.h"
+#include "vulkan.h"
+#include "log.h"
 
 void loop(){
   Window window;
+  if(!window.validateVulkanLayer()) return;
+  testTraingle();
   while(!window.exitFlag){
     window.update();
+    LOGGING::logToOutputStdout();
+    LOGGING::flushLogs();
   }
   
 }
