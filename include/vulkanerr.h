df089d51ee786a3e2f5602338767d228bc52d780
brinq <andrewrdavis18@gmail.com>
2024-10-11 01:23:52 -0700

    aded simple error notification for vulkna

---
+++ include/vulkanerr.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "vulkan/vulkan.h"
+#include "global.h"
+
+inline VkResult gVkResult;
+
+#define VKCALL(x) gVkResult = x; if(gVkResult != VK_SUCCESS){throwVkError(gVkResult, __LINE__, __FILE__);}
+
+inline void throwVkError(VkResult code, int line, const char* file){
+  printf("\033[31m\033[1mVKERROR:\033[0m%i \033[1m\033[32mFILE:\033[0m%s \033[33m\033[1mLINE:\033[0m%i\n",code, file, line );
+  gExitFlag = true;
+  
+}
