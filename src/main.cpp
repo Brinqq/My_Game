aaebb2c1c3fd9b9b9928c562ab91409c916a2c43
brinq <andrewrdavis18@gmail.com>
2024-11-08 06:20:14 -0800

    added impl for query important system information we will need later on

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include "defines.h"
 #include "log.h"
 #include "application.h"
+#include "sysinfo.h"
+#include "unistd.h"
 
 int main(void){
 #if __DEBUG
   LOG_INFO("Current game build running in debug mode!");
 #endif
+  initializeRequiredSystemInfo(1);
   applicationStart();
   return 0;
 }
--- src/system/sysinfo.h
+++ src/system/sysinfo.h
@@ -1,8 +1,22 @@
 #pragma once
+#include "log.h"
 
-#if _UNIX || __APPLE__ 
+
+#if __unix__ || __APPLE__ 
 #include "unistd.h"
+#include <limits.h>
+inline long SYS_PAGE_SIZE;
+inline long SYS_MAX_THREAD_COUNT;
 
-inline const long SYS_PAGE_SIZE = sysconf(_SC_PAGESIZE);
+inline void  initializeRequiredSystemInfo(int log = 0){
+  SYS_PAGE_SIZE = sysconf(_SC_PAGESIZE);
+  SYS_MAX_THREAD_COUNT = sysconf(_SC_THREAD_THREADS_MAX);
+
+  if(__DEBUG && log){
+    printf("\033[1;38;5;208mSYSTEM INFO: Page Size: %lu\033[0m\n", SYS_PAGE_SIZE);
+    printf("\033[1;38;5;208mSYSTEM INFO: Max Threads: %lu\033[0m\n", SYS_MAX_THREAD_COUNT);
+  }
+
+}
 
 #endif
