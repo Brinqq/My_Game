33ab552ef4331f1ca86fd2772d5c5be5da452f75
brinq <andrewrdavis18@gmail.com>
2024-12-27 08:45:36 -0800

    vulkan instance impl

--- src/engine/graphics/core/vulkanb/vulkan.cpp
+++ src/engine/graphics/core/vulkanb/vulkan.cpp
@@ -1,14 +1,48 @@
 #include "vulkan.h"
 #include "platform.h"
 #include "vulkanerror.h"
+#include "vulkandevice.h"
 #include "log.h"
+
 
 #include "bstl/vector.h"
 
 
-static VkInstance g_instance;
 
-static int vulkanCreateInstance(){
+static VkInstance g_instance;
+static VkPhysicalDevice g_gpu;
+
+
+std::array<const char*, 1> requestedLayers{
+  "VK_LAYER_KHRONOS_validation",
+};
+
+
+int checkForRequiredExtensionSupport(){
+  uint32_t extensionCount;
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+  VkExtensionProperties* pAvailableExtensions = (VkExtensionProperties*)alloca(sizeof(VkExtensionProperties)*extensionCount);
+  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, pAvailableExtensions);
+
+  for(int i = 0; i < extensionCount; i++){
+  }
+  
+  return 0;
+}
+
+int checkForRequiredLayerSupport(){
+
+}
+
+static VulkanError vulkanCreateInstance(){
+
+  if(checkForRequiredExtensionSupport()){
+    return VULKAN_ERROR_MISSING_EXTENSION;
+  };
+
+  bstl::Vector<const char*> ext;
+  platformGetRequiredVulkanExtensions(ext);
+  VkInstanceCreateInfo instanceInfo{};
   VkApplicationInfo appInfo{};
   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   appInfo.pApplicationName = "Game";
@@ -16,24 +50,27 @@ static int vulkanCreateInstance(){
   appInfo.pEngineName = "engine";
   appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
   appInfo.apiVersion = VK_API_VERSION_1_0;
-
-  VkInstanceCreateInfo instanceInfo{};
   instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
   instanceInfo.pApplicationInfo = &appInfo;
-  instanceInfo.enabledLayerCount = 0;
-  instanceInfo.ppEnabledLayerNames = nullptr;
-  instanceInfo.flags = PLATFORM_VULKAN_REQUIRED_INSTANCE_FLAGS;
-
-  bstl::Vector<const char*> ext;
-  platformGetRequiredVulkanExtensions(ext);
+  instanceInfo.enabledLayerCount = requestedLayers.size();
+  instanceInfo.ppEnabledLayerNames = requestedLayers.data();
   instanceInfo.enabledExtensionCount = ext.size();
   instanceInfo.ppEnabledExtensionNames = ext.data();
+  instanceInfo.flags = PLATFORM_VULKAN_REQUIRED_INSTANCE_FLAGS;
 
   VKCALL(vkCreateInstance(&instanceInfo, nullptr, &g_instance))
-  return 0;
+
+  return VULKAN_ERROR_NONE;
 }
+
+
+static void cleanup(){
+  vkDestroyInstance(g_instance, nullptr);
+}
+
 
 int vulkanInitialize(){
   vulkanCreateInstance();
+  vulkanCreatePhysicalDevice(g_instance, g_gpu);
   return 0;
 }
